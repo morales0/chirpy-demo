b46c312b842132786ffe477626a8f444e77f15ae
Francisco Morales <francisco@mightyacorn.com>
2026-03-12 10:25:08 -0700

    setting up handlers

--- main.go
+++ main.go
@@ -12,7 +12,13 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir("./")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("./"))))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,

a435ee43bfa78860f60f4315ed0dd8e09c6cdf79
Francisco Morales <francisco@mightyacorn.com>
2026-03-17 15:55:49 -0700

    adding hits handlers

--- main.go
+++ main.go
@@ -1,8 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareIncrement(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	hits := fmt.Sprintf("Hits: %v", cfg.fileServerHits.Load())
+	w.Write([]byte(hits))
+}
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, req *http.Request) {
+	cfg.fileServerHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+}
 
 type apiHandler struct{}
 
@@ -10,15 +34,20 @@ func (apiHandler) ServeHTTP(http.ResponseWriter, *http.Request) {}
 
 func main() {
 	const port = "8080"
+	var cfg apiConfig
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("./"))))
+	mux.Handle("/app/", cfg.middlewareIncrement(http.StripPrefix("/app", http.FileServer(http.Dir("./")))))
 
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
+
+	mux.HandleFunc("/metrics", cfg.metricsHandler)
+	mux.HandleFunc("/reset", cfg.resetHandler)
+
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
